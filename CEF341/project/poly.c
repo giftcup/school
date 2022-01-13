315378b1c7e55a8abb5435167bb399e9c7520638
giftcup <salometambe@gmail.com>
2022-01-13 07:39:07 +0100

    Adding to the list in order

--- CEF341/project/poly.c
+++ CEF341/project/poly.c
@@ -21,77 +21,78 @@ int main(void){
   char condition;
   int degree;
 
-    do {
-        printf("Add node to polynomial (y | n)?: ");
-        scanf("%c", &condition);
-    } while (condition != 'y' || condition != 'Y' || condition != 'n' || condition != 'N');
-
-  while(condition == 'y' || condition == 'Y'){
-    create_poly(&poly1);
     printf("Add node to polynomial (y | n)?: ");
-    scanf(" %c", &condition);
-  }
+    scanf("%c", &condition);
 
-  display_poly(poly1);
-  degree = degree_Poly(poly1);
-  printf("The polynomial is of degree %d\n", degree);
+    while(condition == 'y' || condition == 'Y'){
+        create_poly(&poly1);
+        printf("Add node to polynomial (y | n)?: ");
+        scanf(" %c", &condition);
+    }
 
-  free(poly1);
-  return 0;
+    display_poly(poly1);
+//   degree = degree_Poly(poly1);
+//   printf("The polynomial is of degree %d\n", degree);
+
+    free(poly1);
+    return 0;
 }
 
 void create_node(struct Poly_Node **P) {
     *P = malloc(sizeof(struct Poly_Node));
+    if (*P == NULL) printf("Memory allocation failed in create_node");
 }
 
 void create_poly(struct Poly_Node **P){
-  struct Poly_Node *newNode;
-  create_node(&newNode);
+    struct Poly_Node *newNode, *temp;
+    create_node(&newNode);
 
-  printf("Enter coefficient: ");
-  scanf("%d", &newNode->coefficient);
-  printf("Enter degree: ");
-  scanf("%d", &newNode->degree);
-
-  while(newNode->degree < 0){
-    printf("Degree can not be less than 0, input a correct degree: ");
+    printf("Enter coefficient: ");
+    scanf("%d", &newNode->coefficient);
+    printf("Enter degree: ");
     scanf("%d", &newNode->degree);
-  }
 
-  if(*P == NULL){
-    //first node in the polynomial
-    newNode->next = *P;
-    *P = newNode;
-    (*P)->next = NULL;
-  }
+    while(newNode->degree < 0){
+        printf("Degree can not be less than 0, input a correct degree: ");
+        scanf("%d", &newNode->degree);
+    }
 
-  else {
-    newNode->next = *P;
-    *P = newNode;
-  }
-  free(newNode);
+    if (*P == NULL || newNode->degree > (*P)->degree) {
+        newNode->next = *P;
+        *P = newNode;
+        free(newNode);
+        return;
+    }
+
+    create_node(&temp);
+    temp = *P;
+
+    for(; newNode->degree < temp->degree && temp->next != NULL; temp = temp->next);
+
+    if (newNode->degree == temp->degree) {
+        temp->coefficient += newNode->coefficient;
+        free(newNode);
+        free(temp);
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    free(newNode);
 }
 
 void display_poly(struct Poly_Node *P){
-  if(P == NULL){
-    printf("No polynomial to be displayed.\n");
-    return;
-  }
-  
-  while(P != NULL){
-    if(P->degree > 1){
-      printf("%dx^%d + ", P->coefficient, P->degree);
+    if(P == NULL){
+        printf("No polynomial to be displayed.\n");
+        return;
     }
 
-    else if(P->degree == 1){
-      printf(" %dx + ", P->coefficient);
+    while (P->next != NULL) {
+        printf(" + %dx^%d ", P->coefficient, P->degree);
+        P = P->next;
     }
 
-    else {
-      printf(" %d", P->coefficient);
-    }
-    P = P->next;
-  }
 
   printf(" = 0\n");
 }
