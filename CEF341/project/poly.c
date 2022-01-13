f4919fd0d9b75f267d755b06d1f837ad3fc9f6aa
Blanco237 <asongrandy9@gmail.com>
2022-01-13 21:23:51 +0100

    Completed All Functions(Addition,Multiplication,Evaluation) and added Driver code to test

--- CEF341/project/poly.c
+++ CEF341/project/poly.c
@@ -13,18 +13,23 @@ void make_poly(struct Poly_Node **P);
 void create_poly(struct Poly_Node **);
 void display_poly(struct Poly_Node *);
 int degree_Poly(struct Poly_Node *P);
+void add_to_poly(struct Poly_Node **P, struct Poly_Node *new_node);
 void add_Poly(struct Poly_Node **R, struct Poly_Node *P, struct Poly_Node *Q);
+void add_at_end(struct Poly_Node **R,int coef,int deg);
+void multiply_poly(struct Poly_Node **R,struct Poly_Node *P,struct Poly_Node *Q);
+void removeDuplicates(struct Poly_Node *P);
+int eval_poly(struct Poly_Node *P, int x);
 
 
 int main(void){
     struct Poly_Node *P, *Q, *R;
     P = Q = R = NULL;
     char make_polynomial;
-    int degree;
+    int degree,value;
 
     make_poly(&P);
 
-    printf("Make another polynomial (y|n): ");
+    printf("\n\nMake another polynomial (y | n): ");
     scanf("%c", &make_polynomial);
     fflush(stdin);
     if (make_polynomial == 'y' || make_polynomial == 'Y') {
@@ -39,9 +44,33 @@ int main(void){
         display_poly(Q);
         degree = degree_Poly(Q);
         printf("The polynomial is of degree %d\n", degree);
+        
+        create_node(&R);
+        add_Poly(&R, P, Q);
+        printf("\nSum of Polynomials is : \n");
+        display_poly(R);
+        degree = degree_Poly(R);
+        printf("The polynomial is of degree %d\n",degree);
+        
+        multiply_poly(&R,P,Q);
+        printf("\nThe Product of Polynomials is : \n");
+        display_poly(R);
+        printf("The polynomial is of degree %d\n",degree_Poly(R));
+    }
+    
+    printf("\n\nEnter a value to evaluate Polynomial: ");
+    scanf("%d",&value);
+    fflush(stdin);
+    printf("\nChoose polynomial to evaluate (P | Q): ");
+    scanf("%c", &make_polynomial);
+    fflush(stdin);
+    if (make_polynomial == 'p' || make_polynomial == 'P') {
+        printf("\nThe result is %d\n",eval_poly(P,value));
+    }
+    else if (make_polynomial == 'q' || make_polynomial == 'Q') {
+        printf("\nThe result is %d\n",eval_poly(Q,value));
     }
 
-    free(P);
     return 0;
 }
 
@@ -52,38 +81,21 @@ void create_node(struct Poly_Node **P) {
 
 void make_poly(struct Poly_Node **P) {
     char condition;
-    printf("Add node to polynomial (y | n)?: ");
+    printf("Add node to polynomial (y | n): ");
     scanf("%c", &condition);
     fflush(stdin);
 
     while(condition == 'y' || condition == 'Y'){
         create_poly(P);
-        printf("Add node to polynomial (y | n)?: ");
+        printf("Add node to polynomial (y | n): ");
         scanf(" %c", &condition);
         fflush(stdin);
     }
 }
 
-/**
- * @brief creates a term of a polynomial by getting the
- * degree and coefficient and storing in a structure
- * @param P pointer to a pointer of type struct Poly_Node;
- * @return void
-**/ 
-void create_poly(struct Poly_Node **P){
-    struct Poly_Node *newNode, *cur, *prev;
-    create_node(&newNode);
+void add_to_poly(struct Poly_Node **P, struct Poly_Node *newNode) {
+    struct Poly_Node *cur, *prev;
 
-    printf("Enter coefficient: ");
-    scanf("%d", &newNode->coefficient);
-    printf("Enter degree: ");
-    scanf("%d", &newNode->degree);
-
-    while(newNode->degree < 0){
-        printf("Degree can not be less than 0, input a correct degree: ");
-        scanf("%d", &newNode->degree);
-    }
-    
     if (*P == NULL || newNode->degree > (*P)->degree) {
         newNode->next = *P;
         *P = newNode;
@@ -108,15 +120,36 @@ void create_poly(struct Poly_Node **P){
     }
 
     if (cur != NULL && newNode->degree == cur->degree) {
-        printf("%d %d\n", cur->degree, newNode->degree);
         cur->coefficient += newNode->coefficient;
         return;
     }
 
     newNode->next = cur->next;
     cur->next = newNode;
+}
 
-    free(newNode);
+/**
+ * @brief creates a term of a polynomial by getting the
+ * degree and coefficient and storing in a structure
+ * @param P pointer to a pointer of type struct Poly_Node;
+ * @return void
+**/ 
+void create_poly(struct Poly_Node **P){
+    struct Poly_Node *newNode, *cur, *prev;
+    create_node(&newNode);
+
+    printf("Enter coefficient: ");
+    scanf("%d", &newNode->coefficient);
+    printf("Enter degree: ");
+    scanf("%d", &newNode->degree);
+
+    while(newNode->degree < 0){
+        printf("Degree can not be less than 0, input a correct degree: ");
+        scanf("%d", &newNode->degree);
+    }
+
+    add_to_poly(P, newNode);
+
 }
 
 void display_poly(struct Poly_Node *P){
@@ -125,66 +158,151 @@ void display_poly(struct Poly_Node *P){
         return;
     }
 
+    printf("(%dx^%d)", P->coefficient, P->degree);
+    P = P->next;
     while (P != NULL) {
-        printf(" + %dx^%d ", P->coefficient, P->degree);
+        printf(" + (%dx^%d)", P->coefficient, P->degree);
         P = P->next;
     }
-
-
-  printf(" = 0\n");
+    printf(" = 0\n");
 }
 
 int degree_Poly(struct Poly_Node *P) {
     return P->degree;
 }
 
+
 void add_Poly(struct Poly_Node **R, struct Poly_Node *P, struct Poly_Node *Q){
-	
-	//First loop to add values of R and P while
-	//both exist 
-	while(R->next && P->next){
-		if(R->degree > P->degree){
-			Q->degree = R->degree;
-			Q->coefficient = R->coefficient;
-			R = R->next;
+    int coef,deg;
+    *R = NULL;
+    
+
+	while(Q != NULL && P != NULL) {
+		if (Q->degree > P->degree){
+			deg = Q->degree;
+			coef = Q->coefficient;
+			Q = Q->next;
 		}
-		
-		else if(P->degree > R->degree){
-			Q->degree = P->degree;
-			Q->coefficient = P->coefficient;
+
+		else if(P->degree > Q->degree){
+			deg = P->degree;
+			coef = P->coefficient;
 			P = P->next;
 		}
 		
-		else{
-			Q->degree = R->degree;
-			Q->coefficient = R->coefficient + P->coefficient;
-			R = R->next;
+		else {
+			deg = P->degree;
+			coef = P->coefficient + Q->coefficient;
+			Q = Q->next;
 			P = P->next;
 		}
-		
-//		Q->next = (struct Poly_Node*) malloc(sizeof(struct Poly_Node));
-//		Q = Q->next;
-//		Q->next = NULL;
-		
-		//creates a new node to add next polynomial set
-		create_node(&Q);
+
+		add_at_end(R,coef,deg);
 	}
 	
-	//Second loop to add values of R or P
-	//while R or P exist
-	while(R->next || P->next){
-		if(R->next){
-			Q->degree = R->degree;
-			Q->coefficient = R->coefficient;
-			R = R->next;
+	while(Q != NULL || P != NULL){
+		if(Q->next){
+			deg = Q->degree;
+			coef = Q->coefficient;
+			Q = Q->next;
 		}
 		if(P->next){
-			Q->degree = P->degree;
-			Q->coefficient = P->coefficient;
+			deg = P->degree;
+			coef = P->coefficient;
 			P = P->next;
 		}
 		
-		//creates a new node to add next polynomial set
-		create_node(&Q);
+		add_at_end(R,coef,deg);
+
 	}
+	
+}
+
+void add_at_end(struct Poly_Node **R,int coef,int deg){
+	
+	struct Poly_Node *last,*item;
+	last = (*R);
+	item = (struct Poly_Node*) malloc(sizeof(struct Poly_Node));
+	
+	item->coefficient = coef;
+	item->degree = deg;
+	item->next = NULL;
+	
+	if((*R)==NULL){
+		(*R) = item;
+		return;
+	}
+	
+	while(last->next != NULL){
+		last = last->next;
+	}
+	
+	last->next = item;
+	
+	return;
+}
+
+void multiply_poly(struct Poly_Node **R,struct Poly_Node *P,struct Poly_Node *Q){
+	struct Poly_Node *p1,*p2;
+	int coef,deg;
+	p1 = P;
+	p2 = Q;
+	(*R) = NULL;
+	
+	while(p1 != NULL){
+		while(p2 != NULL){
+			//Multiplying coefficients
+			coef = p1->coefficient * p2->coefficient;
+			//adding degrees
+			deg = p1->degree + p2->degree;
+			
+			add_at_end(R,coef,deg);
+			
+			p2 = p2->next;
+		}
+		p2 = Q;
+		p1 = p1->next;
+	}
+	
+	//Sum coefficients with same power
+	removeDuplicates((*R));
+}
+
+void removeDuplicates(struct Poly_Node *P){
+	struct Poly_Node *p1,*p2,*dup;
+	
+	p1 = P;
+	
+	while(p1 != NULL && p1->next != NULL){
+		p2 = p1;
+		
+		while(p2->next != NULL){
+			//if degree is the same
+			if(p1->degree == p2->next->degree){
+				//adding coefficients
+				p1->coefficient = p1->coefficient + p2->next->coefficient;
+				dup = p2->next;
+				p2->next = p2->next->next;	
+				
+				//removing duplicate
+				free(dup);
+			}
+			else{
+				p2 = p2->next;
+			}
+		}
+		p1 = p1->next;
+	}
+}
+
+int eval_poly(struct Poly_Node *P, int x){
+	int result = 0;
+	
+	while(P != NULL){
+		int temp = P->coefficient * pow(x,P->degree);
+		result+= temp;
+		P = P->next;
+	}
+	
+	return result;
 }
